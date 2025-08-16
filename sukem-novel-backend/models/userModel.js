c660e2fd5d7ebe677f9fbfc4be6012ce65df085c
maihan88 <hanvani0808@gmail.com>
2025-08-16 20:03:35 +0700

    Update userModel.js

--- sukem-novel-backend/models/userModel.js
+++ sukem-novel-backend/models/userModel.js
@@ -1,11 +1,34 @@
+// maihan88/novelweb/novelweb-30378715fdd33fd98f7c1318544ef93eab22c598/sukem-novel-backend/models/userModel.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+
+const bookmarkSchema = new mongoose.Schema({
+    chapterId: { type: String, required: true },
+    progress: { type: Number, required: true },
+    lastRead: { type: Date, required: true },
+}, { _id: false });
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
     createdAt: { type: Date, default: Date.now },
+    // --- BẮT ĐẦU THÊM MỚI ---
+    favorites: {
+        type: [String], // Mảng các storyId
+        default: []
+    },
+    bookmarks: {
+        type: Map,
+        of: bookmarkSchema, // Một Map với key là storyId và value là object Bookmark
+        default: {}
+    },
+    ratedStories: {
+        type: Map,
+        of: Number, // Một Map với key là storyId và value là điểm số (1-5)
+        default: {}
+    }
+    // --- KẾT THÚC THÊM MỚI ---
 });
 
 // Hash password before saving
