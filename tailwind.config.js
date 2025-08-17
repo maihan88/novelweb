67189483159271f8ba491c5d076ae768698259ec
maihan88 <hanvani0808@gmail.com>
2025-08-17 19:01:09 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// maihan88/novelweb/novelweb-d14588e6d469796ca4d76fea103c02df2ebaa5a1/tailwind.config.js
 export default {
   content: [
     "./index.html",
@@ -28,12 +29,24 @@ export default {
             },
             animation: {
               'fade-in': 'fadeIn 0.5s ease-in-out',
+              // --- SỬA TẠI ĐÂY ---
+              'pan-up': 'pan-up 7s ease-in-out forwards',
+              'pan-down': 'pan-down 7s ease-in-out forwards',
+              // --- KẾT THÚC SỬA ĐỔI ---
             },
             keyframes: {
               fadeIn: {
                 '0%': { opacity: 0, transform: 'translateY(10px)' },
                 '100%': { opacity: 1, transform: 'translateY(0)' },
               },
+              'pan-up': {
+                '0%': { backgroundPosition: '50% 80%' },
+                '100%': { backgroundPosition: '50% 20%' },
+              },
+              'pan-down': {
+                  '0%': { backgroundPosition: '50% 20%' },
+                  '100%': { backgroundPosition: '50% 80%' },
+              }
             }
     },
   },
